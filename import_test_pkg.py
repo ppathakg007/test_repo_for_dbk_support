45e87af4866cbbc1c0d2e5779bd33ffb03b646a3
ppathakg007@gmail.com <ppathakg007@gmail.com>
2024-07-11 12:40:16 +0000

    test
    test

--- import_test_pkg.py
+++ import_test_pkg.py
@@ -15,12 +15,20 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install ./
+# MAGIC %sh 
+# MAGIC
+# MAGIC mkdir /tmp/test/test
+# MAGIC cd /Workspace/Repos/.internal/acfc8a97f5_commits/d11290e82172358c6516203b173174044dc5aacd/
+# MAGIC cp -r * /tmp/test/test/
+# MAGIC cd /tmp/test/test/
+# MAGIC
+# MAGIC #cd /Workspace/Repos/praveenkumar.pathak@databricks.com/test_repo_for_dbk_support
+# MAGIC pip install ./
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC ls -l /Workspace/Repos/.internal/f82fad67e9_commits/0ac35f4ddb497eb7dceb0a5a60c44c7641090278/
+# MAGIC ls -l  /tmp/test/test/build/lib/t
 
 # COMMAND ----------
 
@@ -41,4 +49,24 @@ ls -l
 
 # COMMAND ----------
 
-l
+
+cd /Workspace/Repos/.internal/acfc8a97f5_commits/d11290e82172358c6516203b173174044dc5aacd/
+pip install ./
+
+# COMMAND ----------
+
+# MAGIC %sh pwd
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ls -la /Workspace/Repos/
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ls -la /Workspace/Repos/.internal/acfc8a97f5_commits/d11290e82172358c6516203b173174044dc5aacd/*
+
+# COMMAND ----------
+
+
